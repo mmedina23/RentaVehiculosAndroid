d8daff082f780931221c5e9faeb190c74e91081a
Dani18122005 <daniplazagarcia130@gmail.com>
2025-02-10 16:16:59 +0100

    Delete app/src/main/java/com/pmd/rentavehiculos/navegacion/AppScreens.kt

--- app/src/main/java/com/pmd/rentavehiculos/navegacion/AppScreens.kt
+++
@@ -1,7 +0,0 @@
-package com.pmd.rentavehiculos.navegacion
-
-sealed class AppScreens(val ruta: String) {
-    object SplashScreen : AppScreens("splash_screen")
-    object LoginScreen : AppScreens("login_screen")   // Nueva pantalla de login
-    object MainScreen : AppScreens("main_screen")
-}
