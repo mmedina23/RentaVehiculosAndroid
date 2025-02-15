112ca0a5f308335921390f507c06f26243064d5c
Marco-Polo-coding <126604538+Marco-Polo-coding@users.noreply.github.com>
2025-02-15 22:41:10 +0100

    Feat Splash: He añadido el splash

---
+++ app/src/main/java/com/pmd/rentavehiculos/pantallas/Splash.kt
@@ -0,0 +1,55 @@
+package com.pmd.rentavehiculos.pantallas
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.pmd.rentavehiculos.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(Unit) {
+        delay(3000)
+        navController.navigate("login") {
+            popUpTo("splash") { inclusive = true }         }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_playstore),
+                contentDescription = "Logo de la aplicación",
+                modifier = Modifier
+                    .size(300.dp)
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            CircularProgressIndicator(
+                color = Color(0xFF800080),
+                strokeWidth = 5.dp
+            )
+        }
+    }
+}
