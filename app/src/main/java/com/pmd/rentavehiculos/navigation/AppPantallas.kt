0491df5777b6c4b3da2cef72ab17ef1544892a18
KenCastillo <147042780+KenCastillo001@users.noreply.github.com>
2025-02-15 05:34:50 +0100

    Feat Modificacion en las pantallas : Sehan modificado las rutas de navegacion así como las funciones que ejecuta cada pantalla

--- app/src/main/java/com/pmd/rentavehiculos/navigation/AppPantallas.kt
+++ app/src/main/java/com/pmd/rentavehiculos/navigation/AppPantallas.kt
@@ -3,9 +3,9 @@ package com.pmd.rentavehiculos.navigation
 sealed class appPantallas (val ruta :String){
     object splashScreen: appPantallas("splash_screen")
     object LoginScreen: appPantallas("pantalla_login")
-    object MenuScreen: appPantallas("Pantalla_Principal")
-    object ClientScreen: appPantallas("Vista_Cliente")
-    object AdminScreen: appPantallas("Vista_Admin")
+    object MenuPrincipal: appPantallas("Pantalla_Principal")
+    object PantallaCliente: appPantallas("Vista_Cliente")
+    object PantallaAdmin: appPantallas("Vista_Admin")
     object VehiculoScreen: appPantallas("Pantalla-Coches")
     object PersonaScreen: appPantallas("Pantalla-Coches")
 
